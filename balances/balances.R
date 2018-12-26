f34c7f225a50c20cc46a7c7342050aacc0a6be2b
fsphbs <45671218+fsphbs@users.noreply.github.com>
2018-12-26 18:59:37 +0100

    Add files via upload

---
+++ balances/balances.R
@@ -0,0 +1,36 @@
+
+rm(list=ls())
+
+balances <- fread('data/current_balances.txt', sep = ';')
+tickers <- fread('data/counterparts_list.csv')
+
+colnames(balances) <- c('dt', 'counterpart', 'acc.type', 'status', 'ccy', 'amnt')
+balances <- merge.data.frame(x = balances, y = tickers, by = 'counterpart') %>% as.data.table() 
+
+
+balances$dt <- balances$dt %>% as.Date()
+balances$counterpart <- factor(x = balances$counterpart, levels = balances$counterpart %>% unique(), ordered = T)
+balances$ticker <- factor(x = balances$ticker, levels = balances$ticker %>% unique(), ordered = T)
+balances$amnt <- balances$amnt %>% gsub(., pattern = ',', replacement = '.') %>% as.numeric()
+#balances$amnt <- balances$amnt %>% as.numeric()
+balances <- balances[order(dt, counterpart)]
+
+dt.offset <- (balances$dt %>% unique() %>% as.Date()) - (balances$dt %>% unique() %>% min() %>% as.Date())
+
+ccy.list <- unique(balances$ccy)
+counterpart.list <-  unique(balances$counterpart) %>% levels()
+ticker.list <-  unique(balances$ticker) %>% levels()
+dates.list <-  unique(balances$dt)
+
+
+neg.bal <- balances[status=='temporary_balance' & amnt < 0 & dt <= (dt %>% unique %>% extract(7))]
+if(nrow(neg.bal)!=0){dcast(data = neg.bal, formula = ccy + dt ~ ticker, fun.aggregate = sum, value.var = 'amnt') %>% print()}
+
+
+dt <- balances[ccy=='GBP' & acc.type=='current' & status =='temporary_balance' & 
+                 dt <= balances$dt %>% unique() %>% extract(14)]
+
+
+
+
+
