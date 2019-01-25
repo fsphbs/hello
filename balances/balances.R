7a25bac5153be31bf09b5f6209c98977c6c960ca
fsphbs <45671218+fsphbs@users.noreply.github.com>
2019-01-25 19:10:24 +0100

    Add files via upload

--- balances/balances.R
+++ balances/balances.R
@@ -1,6 +1,8 @@
 
 rm(list=ls())
 
+source('code/myFunction.R')
+
 balances <- fread('/FSP/My Documents/Analysis/Treasury/Balances/data/current_balances.txt', sep = ',')
 tickers <- fread('data/counterparts_list.csv')
 
@@ -12,7 +14,6 @@ balances$dt <- balances$dt %>% as.Date()
 balances$counterpart <- factor(x = balances$counterpart, levels = balances$counterpart %>% unique(), ordered = T)
 balances$ticker <- factor(x = balances$ticker, levels = balances$ticker %>% unique(), ordered = T)
 balances$amnt <- balances$amnt %>% gsub(., pattern = ',', replacement = '.') %>% as.numeric()
-#balances$amnt <- balances$amnt %>% as.numeric()
 balances <- balances[order(dt, counterpart)]
 
 dt.offset <- (balances$dt %>% unique() %>% as.Date()) - (balances$dt %>% unique() %>% min() %>% as.Date())
@@ -21,17 +22,65 @@ ccy.list <- unique(balances$ccy)
 counterpart.list <-  unique(balances$counterpart) %>% levels()
 ticker.list <-  unique(balances$ticker) %>% levels()
 dates.list <-  unique(balances$dt)
-
+ticker.list <-  unique(balances$ticker) %>% levels()
+exclude.list <- c('KBBS', 'PF')
 
 neg.bal <- balances[status=='temporary_balance' & amnt < 0 & dt <= (dt %>% unique %>% extract(7))]
 if(nrow(neg.bal)!=0){dcast(data = neg.bal, formula = ccy + dt ~ ticker, fun.aggregate = sum, value.var = 'amnt') %>% print()}
 
 cat('\n')
-open.book <- balances[status=='open_bookings' & acc.type=='current' & amnt !=0 & dt <= (dt %>% unique %>% extract(1))]
-if(abs(open.book$amnt) %>% sum() > 0) { dcast(data = open.book, formula = ccy + dt ~ ticker, fun.aggregate = sum, value.var = 'amnt') %>% print()}
+open.book <- balances[status=='open_bookings' & acc.type=='current' & amnt !=0 & dt <= (dt %>% unique %>% extract(1:2))]
+if(abs(open.book$amnt) %>% sum() > 0) { dcast(data = open.book, formula = dt + ccy ~ ticker, fun.aggregate = sum, value.var = 'amnt') %>% print()}
 
-dt <- balances[ccy=='GBP' & acc.type=='current' & status =='temporary_balance' & 
-                 dt <= balances$dt %>% unique() %>% extract(14)]
+
+
+
+###  BCGE and ZKB CHF and EUR optimization.
+if(TRUE){
+  tmp <- dcast(data = balances[(ccy=='CHF' | ccy=='EUR') & (ticker=='BCGE' | ticker=='ZKB') & 
+                                      status =='temporary_balance' & (acc.type=='current' | acc.type=='margin')  & 
+                                      (balances$ticker %in% (ticker.list %wo% exclude.list))], 
+                    formula = dt ~ ccy + ticker + acc.type, fun.aggregate = sum, value.var = 'amnt')
+  
+  fx.eurchf <- bdp(securities = 'EURCHF Curncy', fields = 'PX_LAST')
+  
+  bcge_zkb <- tmp[, cbind(CHF_BCGE_current, CHF_BCGE_margin,  
+                                      BCGE = (CHF_BCGE_current + CHF_BCGE_margin), EUR_BCGE_current, 
+                                      CHF_ZKB_current, CHF_ZKB_margin, EUR_ZKB_current,  
+                                      ZKB = (CHF_ZKB_current + CHF_ZKB_margin + EUR_ZKB_current * fx.eurchf  %>% as.numeric()))] %>% 
+                      data.table()
+
+  bcge_zkb <- bcge_zkb %>% multiply_by(.001) %>% format(.,nsmall = 1, digits = 1) %>% data.table() 
+  bcge_zkb <- tmp$dt %>% as.character() %>% cbind(date=., bcge_zkb) %>% extract(1:12) 
+
+  colnames(bcge_zkb) <- c('dt', 'GE_CHF_curr', 'GE_CHF_marg', 'BCGE_CHF','BCGE_EUR', 
+                          'ZH_CHF_curr', 'ZH_EUR_marg', 'ZH_EUR_curr', 'ZKB')
+  }
+
+### get FX rates
+if(FALSE){
+  ccy.ticker <- paste(ccy.list[ccy.list != 'CHF' & ccy.list != 'SGR'], 'CHF Curncy', sep = '') 
+  
+  ccy.mkt <- bdp(securities = ccy.ticker, fields = c('PX_LAST', 'QUOTE_FACTOR')) 
+  ccy.pair <- ccy.mkt %>% rownames() %>% substr(start = 1, stop = 3) #%>% c(., 'CHF', 'SGR')
+  
+  ccy.mkt <- ccy.mkt %>% data.table()
+  ccy.mkt <- cbind(as.character(ccy.pair),ccy.mkt)
+  ccy.mkt <- rbindlist(list(ccy.mkt, data.frame('CHF', 1, 1), data.frame('SGR', 1, 1))) 
+  colnames(ccy.mkt) <- c('ccy', 'price', 'factor')
+  ccy.mkt$priceadj <- ccy.mkt[ , (price/factor)]
+  balances <- merge.data.frame(x = balances, 
+                               y = ccy.mkt[,cbind(ccy = (ccy %>% as.character()), fx = as.numeric(priceadj))] %>% data.table(), 
+                               by = 'ccy') %>% data.table()
+  balances$amnt.CHF <- balances[,amnt * as.numeric(fx)]
+  
+  tdy.balances <- balances[dt==dates.list[1] & status=='temporary_balance', sum(amnt.CHF), by=c('ccy', 'acc.type')] %>% data.table()
+  colnames(tdy.balances) <- c('ccy', 'acc.type', 'amnt.CHF')
+}
+
+  
+
+
 
 
 
