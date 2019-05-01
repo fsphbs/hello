43d6e1f31a526a81da7d1fb76e100ce9d9325505
fsphbs <45671218+fsphbs@users.noreply.github.com>
2019-05-01 16:52:10 +0200

    Update mt_reporting.R

--- mixed/mt/mt_reporting.R
+++ mixed/mt/mt_reporting.R
@@ -1,9 +1,10 @@
 
 rm(list=ls())
 
-load(file = 'data/mt_103.Rda')
+load(file = 'data/dt.mt.RDA')
 
-mt.103 <- mt.103[trade.date<as.Date('2019-04-01')]
+###   mt 103 - from 2017 to 2019
+mt.103 <- dt.mt[SWIFT %like% 'SWIFT.103']
 
 mt.103$week <- mt.103$trade.date %>% week()
 mt.103$month <- format(mt.103$trade.date, '%Y%m')
@@ -11,3 +12,5 @@ mt.103$quarter <- quarter(mt.103$trade.date) %>% paste0(year(mt.103$trade.date)-
 mt.103$yr <- mt.103$trade.date %>% year()
 
 
+### by Dispo.bank and by year
+rpt.01 <- dcast(data = mt.103, formula = Dispo.Bank ~ yr, fun.aggregate = length, value.var = 'id')
